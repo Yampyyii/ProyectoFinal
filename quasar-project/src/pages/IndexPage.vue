9760624c8635344f30162bcfe908cf91328fbeb0
Brayan Pallares <ing.pallares69@gmail.com>
2024-11-22 11:44:25 -0600

    codigo corregido. sesion 6

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,95 +1,99 @@
 <template>
   <q-page 
-  v-touch-pan.vertical.prevent.mouse="handlePan"
-  class="flex flex-center text-black">
+    v-touch-pan.vertical.prevent.mouse="handlePan"
+    class="flex flex-center text-black"
+  >
     <div class="row">
       <q-input
-      v-model="data.name"
-      input-class="text-center text-h5 text-red"
-      color="teal"
-      placeholder="Counter"
-      filled
+        v-model="data.name"
+        input-class="text-center text-h5 text-red"
+        color="teal"
+        placeholder="Counter"
+        filled
       />
     </div>
-   <div class="row full-width items-center">
-    <div class="col text-center">
-      <!--botones-->
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn
+          @click="decreaseCounter"
+          v-touch-repeat:300:300:300:300:50.mouse.enter.space="decreaseCounter"
+          icon="remove" 
+          size="xl"
+          round 
+        />
+      </div>
+      <div class="col text-center text-h2">
+        {{ data.counter }}
+      </div>
+      <div class="col text-center">
+        <q-btn
+          @click="increaseCounter"
+          v-touch-repeat:300:300:300:300:50.mouse.enter.space="increaseCounter"
+          icon="add" 
+          size="xl"
+          round 
+        />
+      </div>
+    </div>
+    <div class="row">
       <q-btn
-      @click="decreaseCounter"
-      v-touch-repeat:300:300:300:300:50.mouse.enter.space="decreaseCounter"
-      icon="remove" 
-      size="xl"
-      round 
+        @click="resetCounter"
+        icon="restart_alt" 
+        size="xl"
+        round 
       />
     </div>
-
-    <div class="col text-center text-h2">
-      {{ data.counter }}
-    </div>
-
-    <div class="col text-center">
-      <q-btn
-      @click="increaseCounter"
-      
-      v-touch-repeat:300:300:300:300:50.mouse.enter.space="increaseCounter"
-      icon="add" 
-      size="xl"
-      round 
-      />
-    </div>
-   </div>
-   <div class="row">
-    <q-btn
-    @click="resetCounter"
-      icon="restart_alt" 
-      size="xl"
-      round 
-      />
-   </div>
   </q-page>
 </template>
+
+<script setup>
+import { reactive, watch } from 'vue';
+import { LocalStorage } from 'quasar';
+
+// Data reactiva
+const data = reactive({
+  counter: 0,
+  name: ''
+});
+
+// Recuperar datos almacenados
+const savedData = LocalStorage.getItem('data');
+if (savedData) Object.assign(data, savedData);
+
+// Vigilar cambios y guardar datos
+watch(
+  () => data,
+  (value) => {
+    LocalStorage.set('data', value);
+  },
+  { deep: true }
+);
+
+// Métodos del contador
+const increaseCounter = () => {
+  data.counter++;
+};
+
+const decreaseCounter = () => {
+  if (data.counter > 0) data.counter--;
+};
+
+const resetCounter = () => {
+  data.counter = 0;
+};
+
+const handlePan = (e) => {
+  if (e.delta.y < 0) {
+    increaseCounter();
+  } else {
+    decreaseCounter();
+  }
+};
+</script>
+
 <style scoped>
-.q-page{
+.q-page {
   max-width: 700px;
   margin: 0 auto;
 }
 </style>
-
-
-<script setup>
-/*
-imports
-*/
-import { reactive } from 'vue';
-
-/*data
-*/
-
-const data = reactive({
-counter: 0,
-name:''
-})
-
-/*counter methods
-*/
-
-const increaseCounter = () =>{
-  data.counter++
-}
-
-const decreaseCounter = () =>{
-  if (data.counter >0) data.counter--
-}
-
-const resetCounter = () =>{
-  data.counter= 0
-}
-
- 
-const handlePan = e => {
-    console.log(e.delta.y)
-    if (e.delta.y < 0 ) increaseCounter()
-    else decreaseCounter()
-}
-
-</script>
